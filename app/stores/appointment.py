51caf8d7c3d968bfe37dd2564cd1d1baff19248c
Viresh Panchal <panchalviresh757@gmail.com>
2025-08-06 18:46:23 +0530

    SereGlow

--- app/stores/appointment.py
+++ app/stores/appointment.py
@@ -2,18 +2,21 @@ from app.utils.supabase_client import supabase
 
 
 def add_appointment_store(data, service_id):
-    result = supabase.table("Appointment").insert({
-        "user_id": data.get("user_id"),
-        "service_id": data.get("service_id"),
-        "appointment_date": data.get("appointment_date"),
-        "appointment_time": data.get("appointment_time"),
-        "status": data.get("status")
-    }).execute()
+    if service_id == data.get("service_id"):
+        result = supabase.table("Appointment").insert({
+            "user_id": data.get("user_id"),
+            "service_id": data.get("service_id"),
+            "appointment_date": data.get("appointment_date"),
+            "appointment_time": data.get("appointment_time"),
+            "status": data.get("status")
+        }).execute()
 
-    if result.data and len(result.data) > 0:
-        return result
+        if result.data and len(result.data) > 0:
+            return result
+        else:
+            return {"error": "Failed to create appointment."}, 400
     else:
-        return {"error": "Failed to create appointment."}, 400
+        return {"error": "Failed to create appointment, service_id and data.get('service_id') not match ."}, 400
 
 
 def get_appointment_store(service_id):
@@ -26,21 +29,21 @@ def get_appointment_store(service_id):
         return {"error": f"No appointments found for service_id {service_id}."}, 404
 
 
-def update_appointment_store(data, appointment_id):
+def update_appointment_store(data, service_id, appointment_id):
     result = supabase.table("Appointment").update(
-        data).eq("id", appointment_id).execute()
+        data).eq("id", appointment_id).eq("service_id", service_id).execute()
 
     if result.data and len(result.data) > 0:
         return result
     else:
-        return {"error": f"Appointment with id {appointment_id} not found or not updated."}, 404
+        return {"error": f"Appointment with id {appointment_id} and service_id {service_id} not found or not updated."}, 404
 
 
-def delete_appointment_store(appointment_id):
+def delete_appointment_store(service_id, appointment_id):
     result = supabase.table("Appointment").delete().eq(
-        "id", appointment_id).execute()
+        "id", appointment_id).eq("service_id", service_id).execute()
 
     if result.data and len(result.data) > 0:
         return result
     else:
-        return {"error": f"Appointment with id {appointment_id} not found."}, 404
+        return {"error": f"Appointment with id {appointment_id} and service_id {service_id} not found or not delete."}, 404
