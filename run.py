8fd73db2cf7db7fa53686ea53a6f0cf03c0bba2e
Viresh Panchal <vireshpanchal@Vireshs-MacBook-Air.local>
2025-09-02 15:34:54 +0530

    css update

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     #     for rule in app.url_map.iter_rules():
     #         print(rule)
 
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=8000)
